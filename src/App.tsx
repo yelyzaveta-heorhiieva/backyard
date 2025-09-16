750f26c996f041d088e032a8cbe02cda3434bcf1
Yelyzaveta Heorhiieva <heorhiieva.y@gmail.com>
2025-09-16 18:29:22 +0300

    change appComponent

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,13 @@ import Hero from './components/Hero';
 
 function App() {
   return (
-    <div className='bg-[url(./assets/images/bg-mob.png)] bg-cover md:bg-[url(./assets/images/bg-box.png)] xl:bg-[url(./assets/images/bg-desk.png)] bg-no-repeat bg-[#efeff2]'>
+    <>
       <Header />
-      <main>
+      <main className='bg-[url(./assets/images/bg-mob.png)] bg-cover md:bg-[url(./assets/images/bg-box.png)] xl:bg-[url(./assets/images/bg-desk.png)] bg-no-repeat bg-[#efeff2]'>
         <Hero />
         <AboutSection />
       </main>
-    </div>
+    </>
   );
 }
 
